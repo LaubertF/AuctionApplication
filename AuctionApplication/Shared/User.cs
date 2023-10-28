f669f1dde7aea5e99daa757b05588a3ca432e2c5
LaubertF <laubert.f@gmail.com>
2023-10-28 14:05:53 +0200

    fixes

--- AuctionApplication/Shared/User.cs
+++ AuctionApplication/Shared/User.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic;
+
 namespace AuctionApplication.Shared;
 
 public class User : BaseEntity
@@ -5,5 +7,4 @@ public class User : BaseEntity
     public string Nickname { get; set; } = string.Empty;
     
     public string Auth0Id { get; set; } = string.Empty;
-    public ICollection<Auction> Auctions { get; set; }
 }
