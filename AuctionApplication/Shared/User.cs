2599736d5ac878e7fc87033e79a17e274afb14e6
Martina Mahelyová <m.mahelyova@gmail.com>
2023-11-11 14:06:37 +0100

    uprava stlpcov v entite usera

--- AuctionApplication/Shared/User.cs
+++ AuctionApplication/Shared/User.cs
@@ -4,9 +4,10 @@ namespace AuctionApplication.Shared;
 
 public class User : BaseEntity
 {
-    public string Name { get; set; } = string.Empty;
-    
     public string Auth0Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
 
-    public bool IsAdmin { get; set; } = false;
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
 }
